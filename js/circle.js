770d5698c8d97159a173c6ab1fb3ab14dd67eca1
Beni Bar-Gera <beni@Benis-iMac.local>
2019-02-15 17:12:30 +0100

    back to one circle with opaque backgroudns

--- js/circle.js
+++ js/circle.js
@@ -1,7 +1,15 @@
-window.randomize = function() {
-	$('.radial-progress').attr('data-progress', Math.floor(Math.random() * 100));
+var one = $('#one')
+var two = $('#two')
+function randomize(el) {
+	var percentage = el.attr('data-percentage')
+	el.attr('data-progress', percentage);
 }
-setTimeout(window.randomize, 200);
-$('.radial-progress').click(window.randomize);
+//
+// randomize = function() {
+// 	var percentage = $()
+// 	$('.radial-progress').attr('data-progress', 80);
+// }
+// setTimeout(window.randomize, 200);
+// $('.radial-progress').click(window.randomize);
 
 // Read more here: https://medium.com/@andsens/radial-progress-indicator-using-css-a917b80c43f9
